07e6bb6105eaaaad91c2c5462135940c153f19b7
MattSeligman <matt@modelitup.com>
2022-03-17 17:10:08 -0700

    Applied eslint fixes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,20 @@
 const request = require('request');
 
-const location = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian'
+const location = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian';
 const callbackFunction = (error, response, body)=>{
 
-    // let's check that statusCode response is accepted [200-299]
-    const statusSuccess = (/2[0-9][0-9]/).test(response.statusCode);
+  // let's check that statusCode response is accepted [200-299]
+  const statusSuccess = (/2[0-9][0-9]/).test(response.statusCode);
     
-    // otherwise let's figure out what's causing the denial (some urls could require api keys)
-    if(!statusSuccess){ return console.log("Having trouble accessing the location: ",error)}
+  // otherwise let's figure out what's causing the denial (some urls could require api keys)
+  if (!statusSuccess) {
+    return console.log("Having trouble accessing the location: ",error);
+  }
     
-    console.log(body)
+  console.log(body);
     
-    // returning the body (sourcode or JSON if accessing api)
-    return body;
-}
+  // returning the body (sourcode or JSON if accessing api)
+  return body;
+};
 
-request( location, callbackFunction)
+request(location, callbackFunction);
