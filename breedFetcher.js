7c25529eccae43586104000abb3340491d44a205
MattSeligman <matt@modelitup.com>
2022-03-17 17:47:29 -0700

    Added additional comments to explain thought process

--- breedFetcher.js
+++ breedFetcher.js
@@ -8,14 +8,16 @@ request(location, (error, response, body)=>{
     const statusSuccess = (/2[0-9][0-9]/).test(response.statusCode);
         
     // otherwise let's figure out what's causing the denial (some urls could require api keys)
-    if (!statusSuccess) {
-      return console.log("Having trouble accessing the location: ",error);
-    }
+    if (!statusSuccess) return console.log("Having trouble accessing the location: ", error);
 
+    // assign the jsonObject
     const jsonObject = JSON.parse(body)[0];
+
+    // determine if it's located or not
     const notFound = jsonObject === undefined;
-    if(notFound){ return console.log("I'm having trouble finding that breed. Please") }
+    if(notFound) return console.log("I'm having trouble finding that breed. Please");
     
+    // if its found let's grab that description.
     const description = jsonObject.description
     console.log(description);
 });
