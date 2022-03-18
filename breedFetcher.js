9a9260772295a0f58da8870016d58e7fb13313a9
MattSeligman <matt@modelitup.com>
2022-03-17 17:46:04 -0700

    Added error handling for request to support data and no data

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,21 @@
 const request = require('request');
 
+//const location = 'https://api.thecatapi.com/v1/breeds/search?q=None';
 const location = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian';
-const callbackFunction = (error, response, body)=>{
 
-  // let's check that statusCode response is accepted [200-299]
-  const statusSuccess = (/2[0-9][0-9]/).test(response.statusCode);
-    
-  // otherwise let's figure out what's causing the denial (some urls could require api keys)
-  if (!statusSuccess) {
-    return console.log("Having trouble accessing the location: ",error);
-  }
-    
-  console.log(body);
-    
-  // returning the body (sourcode or JSON if accessing api)
-  return body;
-};
+request(location, (error, response, body)=>{
+    // let's check that statusCode response is accepted [200-299]
+    const statusSuccess = (/2[0-9][0-9]/).test(response.statusCode);
+        
+    // otherwise let's figure out what's causing the denial (some urls could require api keys)
+    if (!statusSuccess) {
+      return console.log("Having trouble accessing the location: ",error);
+    }
 
-request(location, callbackFunction);
+    const jsonObject = JSON.parse(body)[0];
+    const notFound = jsonObject === undefined;
+    if(notFound){ return console.log("I'm having trouble finding that breed. Please") }
+    
+    const description = jsonObject.description
+    console.log(description);
+});
